66b42d1a83f9e8013f65a70629cacb65cef6f795
niksw777 <54086021+niksw777@users.noreply.github.com>
2021-10-11 14:52:31 +0300

    initilize project

---
+++ qrmenu_backend/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
